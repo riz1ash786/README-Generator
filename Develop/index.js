334246a16b6a0d71473d40de6112e514f1a7640d
Rizwan Ashraf <riz1ash786@gmail.com>
2022-02-25 16:07:52 +0000

    starter on index

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require(`./utils/generateMarkdown.js`);
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Enter project title",
+        validate: (input) => {
+            if (input) {
+                return true;
+            } else {
+                return ('Please complete this field');
+                    }
+                }
+            },
+
+
+    
+];
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      name: 'stack',
+      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    },
+    {
+      type: 'list',
+      message: 'What is your preferred method of communication?',
+      name: 'contact',
+      choices: ['email', 'phone', 'telekinesis'],
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
+  );
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
