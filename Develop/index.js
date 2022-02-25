fe3f579e4df6fb0c37c418a208562bdd4af6bbc6
Rizwan Ashraf <riz1ash786@gmail.com>
2022-02-25 16:54:14 +0000

    questions and readme function

--- Develop/index.js
+++ Develop/index.js
@@ -6,54 +6,88 @@ const generateMarkdown = require(`./utils/generateMarkdown.js`);
 // TODO: Create an array of questions for user input
 
 const questions = [
-    {
-        type: "input",
-        name: "title",
-        message: "Enter project title",
-        validate: (input) => {
-            if (input) {
-                return true;
-            } else {
-                return ('Please complete this field');
-                    }
-                }
-            },
+  {
+    type: "input",
+    name: "title",
+    message: "Enter project title",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        return "Please complete this field";
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of the project",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        return "Please complete this field";
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter installation instructions",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        return "Please complete this field";
+      }
+    },
+  },
 
-
-    
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter usage information",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        return "Please complete this field";
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Enter contribution guidelines",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        return "Please complete this field";
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Enter test instructions",
+    validate: (input) => {
+      if (input) {
+        return true;
+      } else {
+        return "Please complete this field";
+      }
+    },
+  },
 ];
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    },
-    {
-      type: 'checkbox',
-      message: 'What languages do you know?',
-      name: 'stack',
-      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
-    },
-    {
-      type: 'list',
-      message: 'What is your preferred method of communication?',
-      name: 'contact',
-      choices: ['email', 'phone', 'telekinesis'],
-    },
-  ])
-  .then((data) => {
-    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
-  });
-  );
-
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  console.log(data);
+  fs.writeFile(fileName, generateMarkdown(data), (err) =>
+    err ? console.log(err) : console.log("success!")
+  );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
