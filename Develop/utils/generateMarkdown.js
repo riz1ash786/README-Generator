0dd3540eebf47b9347422a5f6abdbb8f80c19d64
Rizwan Ashraf <riz1ash786@gmail.com>
2022-02-25 22:34:18 +0000

    license functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    return `![License](${license.badgeUrl})`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    return `![License](${license.licenseLink})`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `${license}`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  ${renderLicenseBadge(data.license)}
 
   ## Table of contents
   - [Description](#description)
@@ -42,6 +59,7 @@ function generateMarkdown(data) {
   ${data.test}
 
   ## License
+  ${renderLicenseBadge(data.license)}
   ${renderLicenseSection(data.license)}
 
   ## The URL of the deployed application
